3105f3e599000999051d3f081373319c6eaad2da
Emma Spencer <emma.n.spencer@gmail.com>
2013-10-29 20:46:28 -0400

    methods work

--- bank.js
+++ bank.js
@@ -5,12 +5,17 @@ var Account = function (owner, current_balance) {
 		this.current_balance = this.current_balance + x;
 		return this.current_balance;
 		console.log(this.current_balance);
-	},
+	};
+
 	this.withdraw = function (x) {
-		this.current_balance = this.current_balance - x;
-		return this.current_balance;
-		console.log(this.current_balance);
-	}
+		if (this.current_balance = this.current_balance - x) {
+			return this.current_balance;
+			console.log(this.current_balance);
+		}
+		else {
+			return "You don't have enough money for the withdrawal."
+		}
+	};
 };
 
 var Bank = {
@@ -23,18 +28,15 @@ var Bank = {
 		{owner: "Bryce", current_balance: 6400 }
 	],
 	total_sum: function () {
-		var total_sum;
-		for (var i = 0; i < this.accounts.length; i = i + 1)
-		{
-			var total_sum = 0;
-			total_sum = total_sum + this.account[i].current_balance;
-			return total_sum
-		}
-		console.log(this.total_sum);
+		var balances = this.accounts.map(function(account){ return account.current_balance});
+		var all_sum = 0;
+		balances.forEach(function(value){ all_sum += value});
+		return all_sum
 	},
-	enroll_new_account: function (x, y) {
-		new_account = new Account(x, y);
+	enroll_new_account: function (owner, current_balance) {
+		var new_account = new Account(owner, current_balance);
 		this.accounts.push(new_account);
+		return new_account;
 		console.log(accounts);
 	}
 }
