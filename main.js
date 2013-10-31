4929ffdc550c0d5f646485ba2df8d426057893a2
Dan Crews <dancrews@me.com>
2013-10-31 12:43:07 -0400

    Completed HW

--- main.js
+++ main.js
@@ -4,39 +4,41 @@ var Account = function(current_balance, owner_name) {
 
 	this.deposit = function(deposit) {
 		return current_balance = current_balance + deposit;
-	};
+	},
 
 	this.withdrawal = function(withdrawal) {
 		return current_balance = current_balance - withdrawal;
-	};
+	}
 
 };
 
 var Bank = {
 	accounts: [],
 
-	enroll_new_account: function(current_balance, owner_name) {
-			var new_account = new Account(current_balance, owner_name);
-	    this.accounts.push(new_account);
-	    return new_account;
+	enroll_new_account: function(account) {
+	    this.accounts.push(account);
 	},
 
-	total_deposits: function(accounts) {
-	  var balances = this.accounts.map( function(account) { return account.balance });
-    return balances.reduce(function(a, b) { return a + b });
-	}
-
-
+	total_deposits: function() {
+    return this.accounts.map(function(account){ return account.current_balance; }).reduce(function(x, y){ return x + y; });
+  }
 };
 
 
 
 // new accounts
-dans_acct = new Account("Dan", 1000);
-cindys_acct = new Account("Cindy", 1000);
+dans_acct = new Account(1000, "Dan");
+cindys_acct = new Account(1000, "Cindy");
+
 
 // new bank
 Bank.enroll_new_account(dans_acct);
 Bank.enroll_new_account(cindys_acct);
 
-console.log(Bank.total_deposits)
+
+
+console.log(dans_acct.current_balance);
+console.log(cindys_acct.current_balance);
+
+
+console.log(Bank.total_deposits());
